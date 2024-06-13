94dcc00d09e5e4d971707c1011ab268b8a32829b
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-13 20:16:00 +0100

    resolved the urls and views,  will definitely get back to more

--- core/views.py
+++ core/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render, redirect
-from .forms import FileUploadForm, AnalysisPromptForm
+from django.shortcuts import redirect, render
+
+from .forms import AnalysisPromptForm, FileUploadForm
+
 
 def HomePage(request):
-  return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def ChatPage(request):
-  if request.method == 'POST':
-    form = FileUploadForm(request.POST, request.FILES)
-    if form.is_valid():
-      uploaded_file = form.save()
-      # return redirect('analyze_file', file_id=uploaded_file.id)
-  else:
-    form = FileUploadForm()
-  return render(request, 'chat.html', {'form': form})
+    if request.method == "POST":
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = form.save()
+            # return redirect('analyze_file', file_id=uploaded_file.id)
+    else:
+        form = FileUploadForm()
+    return render(request, "chat.html", {"form": form})
+
 
 def AboutView(request):
-  return render(request, "about.html")
+    return render(request, "about.html")
