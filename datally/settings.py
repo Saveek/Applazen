97071e45abbcf5339576d0cfc30161c02d09e462
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-18 15:22:52 +0100

    copied transaction file

--- datally/settings.py
+++ datally/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,10 +113,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'core/static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR/'core', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'core/media')
 
 
 # Default primary key field type
