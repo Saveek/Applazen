0b885e716ec5006e5a40db3532839edaff64eb02
Chukwuebuka Onyemelukwe <saveek4@gmail.com>
2024-06-19 15:56:07 +0100

    refactor docker

--- core/urls.py
+++ core/urls.py
@@ -10,14 +10,15 @@ urlpatterns = [
     path("chat/", views.ChatPage, name="chat"),
     path("home/", views.HomeView, name="home"),
     path("about/", views.AboutView, name="about"),
-    path("account/", views.AccountsView, name="accounts"),
+    path("accounts/", views.AccountsView, name="accounts"),
     path("base/", views.BaseView, name="base"),
     path("budget/", views.BudgetView, name="budget"),
     path("feature/", views.FeaturesView, name="features"),
-    path("goal/", views.GoalsView, name="goals"),
+    path("goals/", views.GoalsView, name="goals"),
     path("intelligence/", views.IntelligenceView, name="intelligence"),
     path("investments/", views.InvestmentsView, name="investments"),
-    path("landing/", views.LandingView, name="landing"),
+    path("landing1/", views.LandingView, name="landing"),
+    #path("landing1/", views.Transactions, name="landing"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
