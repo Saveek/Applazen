762a4afce79b21fcf80055752d07ecbadda2a4b5
Saveek <53392279+Saveek@users.noreply.github.com>
2024-06-20 15:04:52 +0100

    Delete gitpushtask.sh

--- gitpushtask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 5  # Wait for five seconds
-echo "Running script at $(date)" >> /home/abba/Desktop/Applazen/script.log
-cd
-cd /home/abba/Desktop/Applazen
-./git_push.sh >> /home/abba/Desktop/Applazen/script.log
