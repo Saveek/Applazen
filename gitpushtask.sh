7c5e4390a789cb3cb31d2a1ec8cfa03088231df2
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-20 12:30:00 +0100

    hot fix

--- gitpushtask.sh
+++ gitpushtask.sh
@@ -3,4 +3,4 @@ sleep 5  # Wait for five seconds
 echo "Running script at $(date)" >> /home/abba/Desktop/Applazen/script.log
 cd
 cd /home/abba/Desktop/Applazen
-./git_push.sh 
+./git_push.sh >> /home/abba/Desktop/Applazen/script.log
