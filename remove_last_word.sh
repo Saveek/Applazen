d3fb5ee954e3975571d88655d4efd87b87489450
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-20 10:34:40 +0100

    hot fix

--- remove_last_word.sh
+++ remove_last_word.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 5  # Wait for five seconds
 echo "Running script at $(date)" >> /home/abba/Desktop/Applazen/script.log
-sed -i '$ s/.$//' /home/abba/Desktop/Applazen/random.html >> /home/abba/Desktop/Applazen/script.log 2>&1
+sed -i '$ s/.$//' /home/abba/Desktop/Applazen/add.html >> /home/abba/Desktop/Applazen/script.log 2>&1
