4ebace99be123f0c75460192c179e9d61a1ea5f6
Saveek <53392279+Saveek@users.noreply.github.com>
2024-06-20 15:03:21 +0100

    Delete remove_last_word.sh

--- remove_last_word.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sleep 5  # Wait for five seconds
-echo "Running script at $(date)" >> /home/abba/Desktop/Applazen/script.log
-sed -i '$ s/.$//' /home/abba/Desktop/Applazen/add.html >> /home/abba/Desktop/Applazen/script.log 2>&1
